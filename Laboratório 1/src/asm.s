513499d882404f1485f4f7109d57e555df2341e3
Renato Böhler <renato.bohler@gmail.com>
2018-03-29 20:20:08 -0300

    corrige implementação do assembly

--- Laboratório 1/src/asm.s
+++ Laboratório 1/src/asm.s
@@ -4,58 +4,61 @@
         THUMB
         
 meanfilter3:
-        sub     R12,R0,#2    //dim_x-2 (out_x)
-        sub     R6,R1,#2    //dim_y-2
-        lsl     R8,R0,#1    //dim_x*2 (dbl_x)
+        sub     R12,R0,#2               // dim_x-2 (out_x)
+        sub     R6,R1,#2                // dim_y-2
+        lsl     R8,R0,#1                // dim_x*2 (dbl_x)
         mul     R9,R0,R6    
-        sub     R9,R9,#2    //dim_x*(dim_y - 2) - 2    (max_i)
+        sub     R9,R9,#2                // dim_x*(dim_y - 2) - 2    (max_i)
         
-        mov     R10,#0      // i = 0
-        mov     R6,#0       // j = 0
+        mov     R10,#0                  // i = 0
+        mov     R6,#0                   // j = 0
 loop:
         sub     R5,R9,R10
-        cbz     R5, fim    //sai do loop se i == max_i
-        // TODO: pular pro dont quando if (i % dim_x < out_x) nao passar
-        mov     R5,R10      //cria um i temporario
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i]
-        add     R5,R5,#1    //i+1
-        mov     R4,R7       //acc += img_in[i]
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+1]
-        add     R5,R5,#1    //i+2
-        add     R4,R4,R7    //acc += img_in[i+1]
-        ldrb    R7,[R2,R5] //coloca em r7 img_in[i+2]
-        add     R4,R4,R7    //acc += img_in[i+2]
-        mov     R5,R10      //i
-        add     R5,R5,R0    //i+dim_x
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+dim_x]
-        add     R5,R5,#1    //i+dim_x+1
-        add     R4,R4,R7    //acc += img_in[i+dim_x]
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+dim_x+1]
-        add     R5,R5,#1    //i+dim_x+2
-        add     R4,R4,R7    //acc += img_in[i+dim_x+1]
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+dim_x+2]
-        add     R4,R4,R7    //acc += img_in[i+dim_x+2]
-        mov     R5,R10      //i
-        add     R5,R5,R8    //i+dbl_x
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+dbl_x]
-        add     R5,R5,#1    //i+dbl_x+1
-        add     R4,R4,R7    //acc += img_in[i+dbl_x]
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+dbl_x+1]
-        add     R5,R5,#1    //i+dbl_x+2
-        add     R4,R4,R7    //acc += img_in[i+dbl_x+1]
-        ldrb    R7,[R2,R5]  //coloca em r7 img_in[i+dbl_x+2]
-        add     R4,R4,R7    //acc += img_in[i+dbl_x+1]
-        mov     R5,#9       //coloca no i_tmp o divisor, já que ele nao é mais usado até o fim dessa iteração
-        udiv    R4,R4,R5   //acc/9
-        strb    R4,[R3,R10]        //img_out[i]=(acc/9)       
-        add     R6,R6,#1    //j++
+        cbz     R5, fim                 // sai do loop se i == max_i
+        udiv    R5, R10, R0
+        mls     R5, R0, R5, R10         // R5 = i % dim_x
+        cmp     R5, R12                 // compara (i % dim_x) com out_x
+        bcs     dont                    // pula para dont se (i % dim_x >= out_x)
+        mov     R5,R10                  // cria um i temporario
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i]
+        add     R5,R5,#1                // i+1
+        mov     R4,R7                   // acc += img_in[i]
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+1]
+        add     R5,R5,#1                // i+2
+        add     R4,R4,R7                // acc += img_in[i+1]
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+2]
+        add     R4,R4,R7                // acc += img_in[i+2]
+        mov     R5,R10                  // i
+        add     R5,R5,R0                // i+dim_x
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+dim_x]
+        add     R5,R5,#1                // i+dim_x+1
+        add     R4,R4,R7                // acc += img_in[i+dim_x]
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+dim_x+1]
+        add     R5,R5,#1                // i+dim_x+2
+        add     R4,R4,R7                // acc += img_in[i+dim_x+1]
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+dim_x+2]
+        add     R4,R4,R7                // acc += img_in[i+dim_x+2]
+        mov     R5,R10                  // i
+        add     R5,R5,R8                // i+dbl_x
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+dbl_x]
+        add     R5,R5,#1                // i+dbl_x+1
+        add     R4,R4,R7                // acc += img_in[i+dbl_x]
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+dbl_x+1]
+        add     R5,R5,#1                // i+dbl_x+2
+        add     R4,R4,R7                // acc += img_in[i+dbl_x+1]
+        ldrb    R7,[R2,R5]              // coloca em r7 img_in[i+dbl_x+2]
+        add     R4,R4,R7                // acc += img_in[i+dbl_x+1]
+        mov     R5,#9                   // coloca no i_tmp o divisor, já que ele nao é mais usado até o fim dessa iteração
+        udiv    R4,R4,R5                // acc/9
+        strb    R4,[R3,R6]              // img_out[i]=(acc/9)       
+        add     R6,R6,#1                // j++
 dont:
-        add     R10,R10,#1  //i++
+        add     R10,R10,#1              // i++
         b       loop
 fim:
-        sub     R6,R1,#2    //dim_y-2 (repetindo pq faltou registrador)
-        mul     R7,R12,R6    //tam = (dim_x - 2)*(dim_y - 2)
-        mov     R0,R7       //coloca o tamanho de img_out no retorno(R0)
+        sub     R6,R1,#2                // dim_y-2 (repetindo pq faltou registrador)
+        mul     R7,R12,R6               // tam = (dim_x - 2)*(dim_y - 2)
+        mov     R0,R7                   // coloca o tamanho de img_out no retorno(R0)
 
         bx      lr
 
--- Laboratório 1/src/main.c
+++ Laboratório 1/src/main.c
@@ -4,21 +4,14 @@ extern long int meanfilter3(int dim_x, int dim_y, char* img_in, char* img_out);
 
 int main(){
 
-  char img_in[] = {10, 01, 01, 02, 20,
-                   01, 01, 01, 03, 04,
-                   01, 01, 01, 04, 04,
-                   02, 03, 04, 04, 05,
-                   03, 04, 04, 05, 05,
-                   30, 04, 05, 05, 40};
+  char *img_in = malloc (160 * 120 * sizeof (char));
 
-  int dim_x = 5;
-  int dim_y = 6;
+  int dim_x = 160;
+  int dim_y = 120;
   
   int tam = (dim_x-2) * (dim_y-2);
   char* img_out = malloc (tam * sizeof (char));
-  long int j = meanfilter3(dim_x, dim_y, img_in, img_out); //to do
-  free (img_out);
-  img_out = NULL;
+  long int j = meanfilter3(dim_x, dim_y, img_in, img_out);
   
   return j;
 }
