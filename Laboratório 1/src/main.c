9afe2bda6f8dfc1642eb1fb9c574458624bde1f5
Renato Böhler <renato.bohler@gmail.com>
2018-03-29 21:11:30 -0300

    adiciona leitura de arquivos pgm

--- Laboratório 1/src/main.c
+++ Laboratório 1/src/main.c
@@ -1,13 +1,33 @@
-extern long int meanfilter3(int dim_x, int dim_y, char* img_in, char* img_out);
-
+#include <stdio.h>
 #include <stdlib.h>
+
+extern long int meanfilter3(int dim_x, int dim_y, char* img_in, char* img_out);
 
 int main(){
 
-  char *img_in = malloc (160 * 120 * sizeof (char));
-
-  int dim_x = 160;
-  int dim_y = 120;
+  FILE *fIn = fopen("img_in/f14.ascii.pgm", "r");
+  // TODO: fazer escrever a imagem de saída
+  FILE *fOut = fopen("img_out/out.ascii.pgm", "ab+");
+  
+  char value;
+  int offset = 0;
+  
+  // Desconsidera magic word e o nome da imagem
+  fscanf(fIn, "%s\n", &value);
+  fscanf(fIn, "%s\n", &value);
+  
+  int dim_x, dim_y;
+  fscanf(fIn, "%d %d\n", &dim_x, &dim_y);
+  // Desconsidera o máximo valor
+  fscanf(fIn, "%s\n", NULL);
+  
+  char *img_in = malloc (dim_x * dim_y * sizeof (char));
+  
+  while(!feof(fIn)){
+    fscanf(fIn, "%hu", &value);
+    fprintf(fOut, "%hu", value);
+    img_in[offset] = value;
+  }
   
   int tam = (dim_x-2) * (dim_y-2);
   char* img_out = malloc (tam * sizeof (char));
